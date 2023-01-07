4dfa654421bf1b231ca70f613b22ea0e53136daa
mrcaidev <mrcaidev@gmail.com>
2023-01-07 19:30:37 +0800

    feat: add health check endpoint

--- backend/app.py
+++ backend/app.py
@@ -20,3 +20,8 @@ def get_answer():
         abort(jsonify({"error": "问题不能为空"}), 400)
 
     return {"answer": qa.answer(question)}
+
+
+@app.get("/healthz")
+def check_health():
+    return {"message": "ok"}
