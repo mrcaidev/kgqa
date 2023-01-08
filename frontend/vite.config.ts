5df917f66349ffe25ab6df15db56205d92955114
mrcaidev <mrcaidev@gmail.com>
2023-01-08 12:24:02 +0800

    feat: bootstrap frontend with vite + preact

---
+++ frontend/vite.config.ts
@@ -0,0 +1,7 @@
+import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [preact(), tsconfigPaths()],
+});
